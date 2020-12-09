92cc0b7cd6fb7ec23aa7a8bd23b3f63dd90b76cc
max-las <lasserre.maxime@yahoo.fr>
2020-12-09 21:11:52 +0100

    Fix mécanique bouton recherche + carte commerce au propre

--- assets/js/btnrech.js
+++ assets/js/btnrech.js
@@ -1,12 +1,31 @@
-const btnr = document.querySelector('.btnrech');
+rechercheOuverte = false;
 
-btnr.addEventListener('click', function (event){
+function ajustRecherche(){
+    $("#searchDiv").height("calc(100% - "+($("#titleDiv").innerHeight()+$("#searchArea").innerHeight())+"px)");
+}
+
+function fermeRecherche(){
+    $("#searchDiv").height("calc(100% - "+$("#titleDiv").innerHeight()+"px)");
+}
+
+$(".btnrech").click(function(){
     
     if( $("#formDiv").hasClass("show") ){
         $("#formDiv").removeClass("show");
     }else{
-        const panel = document.querySelector('#searchDiv');
-        panel.classList.add('down');
+        if(rechercheOuverte){
+            fermeRecherche();
+            rechercheOuverte = false;
+        }else{
+            ajustRecherche();
+            rechercheOuverte = true;
+        }
     }
 
-})
+});
+
+$(window).resize(function(){
+    if(rechercheOuverte){
+        ajustRecherche();
+    }
+});
