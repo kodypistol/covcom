042625f0a76e129543cbf554ecfb5f6520d87e33
Adel Sanaa <adel.sanaa@mmibordeaux.com>
2020-12-10 18:00:39 +0100

    Modifications site web adel (Home page boutons, pop up home page, formulaire

--- assets/js/carte_commerce.js
+++ assets/js/carte_commerce.js
@@ -1,4 +1,4 @@
-var btnPopup = document.getElementById('btnDecouvrir');
+/*var btnPopup = document.getElementById('btnDecouvrir');
 var overlay = document.getElementById('overlay');
 btnPopup.addEventListener('click',openMoadl);
 function openMoadl() {
@@ -9,4 +9,12 @@ var btnClose = document.getElementById('btnClose');
 btnClose.addEventListener('click',closeModal);
 function closeModal() {
     overlay.style.display='none';
-    }
+    }*/
+
+    $(".bouton_decouvrir").click(function(){
+        $("#overlay").show();
+    });
+    
+    $("#btnClose").click(function(){
+        $("#overlay").hide();
+    });
