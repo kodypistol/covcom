5f8143c4a6bfd2343bdd203a2e1f670b72703553
0ur0boss <enzo.duret@yahoo.fr>
2020-12-10 18:06:10 +0100

    changementtextbaretcouleur

--- assets/js/formulaire.js
+++ assets/js/formulaire.js
@@ -18,6 +18,7 @@ var form = document.querySelector('#formDiv');
 var inputs = form.querySelectorAll('input, textarea');
 var percentElement = form.querySelector('.percentage');
 var progresstext = form.querySelector('.progressiontext');
+var txtprogr = form.querySelector('.txtprog');
 function checkProgression() {
     var progress = 0;
 
@@ -31,12 +32,15 @@ function checkProgression() {
 
     var percent = (progress / inputs.length) * 100;
     percentElement.style.width = Math.round(percent) + '%';
-    progresstext.innerText = Math.round(percent) + '%';
+
+    progresstext.innerText = "Votre Progression : " + Math.round(percent) + '%';
     let coulg = false;
     // percentElement.style.background= #0B9710;
     console.log(percent)
     if (percent > 80 && coulg === false) {
         percentElement.classList.add('green');
+        progresstext.innerText = "Vous avez bientot fini ! : " + Math.round(percent) + '%';
+
         coulg = true;
       // percentElement.style.color = "#0B9710"
     } else {
