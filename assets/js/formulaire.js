bd64127a49da19c054c910b8876a604ba424b356
0ur0boss <enzo.duret@yahoo.fr>
2020-12-10 17:17:44 +0100

    pull17h17

--- assets/js/formulaire.js
+++ assets/js/formulaire.js
@@ -17,23 +17,35 @@ function countChar(val) {
 var form = document.querySelector('#formDiv');
 var inputs = form.querySelectorAll('input, textarea');
 var percentElement = form.querySelector('.percentage');
-function checkProgression () {
+function checkProgression() {
     var progress = 0;
 
-    inputs.forEach(function(input){
-        if (input.value.length > 0){
+    inputs.forEach(function (input) {
+        if (input.value.length > 0) {
             progress += 1;
         }
     });
 
-    console.log(progress)
+    
 
-    var percent = (progress/inputs.length)*100;
-    percentElement.style.width=percent+'%';
-}
+    var percent = (progress / inputs.length) * 100;
+    percentElement.style.width = percent + '%';
+    let coulg = false;
+    // percentElement.style.background= #0B9710;
+    console.log(percent)
+    if (percent > 80 && coulg === false) {
+        percentElement.classList.add('green');
+        coulg = true;
+      // percentElement.style.color = "#0B9710"
+    } else {
+        percentElement.classList.remove('green');
 
-inputs.forEach(function(input){
-    input.addEventListener('input',function(){
+    }
+};
+
+
+inputs.forEach(function (input) {
+    input.addEventListener('input', function () {
         checkProgression()
     });
 });
